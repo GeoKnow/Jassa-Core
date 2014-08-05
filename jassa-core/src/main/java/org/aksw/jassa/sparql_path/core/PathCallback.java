7c3d5ecafb8bde733c2a035b5585182f842631f8
Tim Ermilov <yamalight@gmail.com>
2014-08-05 12:37:29 +0200

    change project structure + refactor rdf and vocab into commonjs

--- jassa-core/src/main/java/org/aksw/jassa/sparql_path/core/PathCallback.java
+++
@@ -1,7 +0,0 @@
-package org.aksw.jassa.sparql_path.core;
-
-import org.aksw.jassa.sparql_path.core.domain.Path;
-
-public interface PathCallback {
-	void handle(Path path);
-}
--- jassa-js/src/main/webapp/resources/js/facete/facete-query-plan.js
+++
@@ -1,7 +0,0 @@
-(function() {
-
-	// This purpose of this file is just to collect some ideas yet.
-	// In general it seems like a good idea to be able to represent sparql workflows in some kind of object structure.
-	
-	
-})();
