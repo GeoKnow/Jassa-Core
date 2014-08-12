a1326d75d94ba6a69ca84527f3bfa0fda564f737
Tim Ermilov <yamalight@gmail.com>
2014-08-12 13:17:25 +0200

    add coveralls badge

---
+++ gulp/tasks/coveralls.js
@@ -0,0 +1,17 @@
+// var cmd = 'istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage';
+
+var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+var shell = require('gulp-shell');
+
+module.exports = function() {
+    return gulp.src(['lib/**/*.js'])
+        .pipe(istanbul()) // Covering files
+        .on('finish', function() {
+            gulp.src(['test/**/*.js'])
+                .pipe(mocha({reporter: 'spec', report: 'lcovonly'}))
+                .pipe(istanbul.writeReports())
+                .pipe(shell('cat ./coverage/lcov.info | ./node_modules/.bin/coveralls')); // Creating the reports after tests runned
+        });
+};
