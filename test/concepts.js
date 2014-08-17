e0f378f9d67b0aa06b0953708f5a21a5a41c2775
cstadler <cstadler@informatik.uni-leipzig.de>
2014-08-17 14:31:16 +0200

    More work

--- test/concepts.js
+++ test/concepts.js
@@ -36,12 +36,14 @@ describe('Concept Operations', function() {
     it('#Keyword Search Concept Combination', function() {
         var baseConcept = sparql.ConceptUtils.createTypeConcept('http://dbpedia.org/ontology/Person');
 
-        var labelRelation = sparql.KeywordSearchUtils.createRelationPrefLabels();
+        var bestLabelConfig = new sparql.BestLabelConfig();
+
+        var labelRelation = sparql.LabelUtils.createRelationPrefLabels(bestLabelConfig);
         var filterConcept = sparql.KeywordSearchUtils.createConceptRegex(labelRelation, 'Claus');
 
         var combinedConcept = sparql.ConceptUtils.createCombinedConcept(baseConcept, filterConcept);
 
-       var expected = '?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Person> . Optional {?s ?y ?z . Filter((?y in (<http://www.w3.org/2000/01/rdf-schema#label>))) . Filter((langMatches(lang(?z), "en") || langMatches(lang(?z), ""))) . Filter(regex(str(?z), "Claus", "i"))} . Filter((regex(str(?s), "Claus", "i") || bound(?z))); ?s';
+        var expected = '?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Person> . Optional {?s ?y ?z . Filter((?y in (<http://www.w3.org/2000/01/rdf-schema#label>))) . Filter((langMatches(lang(?z), "en") || langMatches(lang(?z), ""))) . Filter(regex(str(?z), "Claus", "i"))} . Filter((regex(str(?s), "Claus", "i") || bound(?z))); ?s';
 
         combinedConcept.toString().should.equal(expected);
     });
