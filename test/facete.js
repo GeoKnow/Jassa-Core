22507a1ffcee5673544c8b5cec5c42bed7bbd1b6
cstadler <cstadler@informatik.uni-leipzig.de>
2014-08-22 18:49:50 +0200

    Added sparqljs, work on facet-tables

--- test/facete.js
+++ test/facete.js
@@ -27,128 +27,128 @@ var sponate = jassa.sponate;
 var facete = jassa.facete;
 var util = jassa.util;
 
+
+function prettifyFacetTreeChildren(children) {
+    var root = {
+        labelInfo: { displayLabel: 'root' },
+        countInfo: {hasMoreItems: true},
+        outgoing: children
+    };
+
+    var result = prettifyFacetTree(root);
+    return result;
+};
+
+function prettifyFacetTree(node) {
+    var ci = node.countInfo;
+    var li = node.labelInfo || {};
+
+    var children = node.outgoing || node.incoming || [];
+
+    var dir = node.outgoing ? '->' : '<-';
+    var label = li.displayLabel || node.id.getUri();
+    var count = ci.hasMoreItems ? '*' : ci.count;
+
+    var str = dir + ' ' + label + ' (' + count + ')';
+
+    var cs = children.map(prettifyFacetTree);
+
+    var result = {
+        label: str,
+        children: cs
+    };
+    return result;
+};
+
 // tests
 describe('Facete Basics', function() {
+
     it('#Service test', function() {
 
-        // Sparql service init
-        var sparqlService = new service.SparqlServiceHttp('http://fp7-pp.publicdata.eu/sparql', ['http://fp7-pp.publicdata.eu/']);
+        if(false) {
+            var sparqlService = new service.SparqlServiceHttp('http://fp7-pp.publicdata.eu/sparql', ['http://fp7-pp.publicdata.eu/']);
+            sparqlService = new service.SparqlServiceConsoleLog(sparqlService);
+
+            var facetSystem = new facete.FacetSystemSparql(sparqlService);
+            var constraintManager = new facete.ConstraintManager();
+            var facetService = facetSystem.createFacetService(constraintManager);
+
+            var ftc = new facete.FacetTreeConfig();
+            ftc.setState(new facete.Path(), new facete.FacetNodeState(1, 10, null, 'funding'));
+
+            ftc.setState(facete.Path.parse('http://fp7-pp.publicdata.eu/ontology/funding'), new facete.FacetNodeState(1, 10, null, null));
+
+
+            facete.FacetTreeService.fetchFacetTree(facetService, ftc).then(function(items) {
+
+                var json = prettifyFacetTreeChildren(items);
+                console.log('TREE: ' + JSON.stringify(json, null, 4));
+            });
+        }
+    });
+
+    it('#Resource list', function() {
+
+        var sparqlService = new service.SparqlServiceHttp('http://datacat.aksw.org/sparql', []);
         sparqlService = new service.SparqlServiceConsoleLog(sparqlService);
+
+        var facetConfig = new facete.FacetConfig();
+        var cpath = facete.Path.parse('http://fp7-pp.publicdata.eu/ontology/funding http://fp7-pp.publicdata.eu/ontology/partner http://fp7-pp.publicdata.eu/ontology/address http://fp7-pp.publicdata.eu/ontology/country');
+        facetConfig.getConstraintManager().addConstraint(new facete.ConstraintEquals(cpath, rdf.NodeFactory.createUri('http://foo.org/foo')));
+        var path = facete.Path.parse('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+        var excludeSelfConstraints = false;
+        var concept = facete.FacetUtils.createConceptResources(facetConfig, cpath, excludeSelfConstraints);
+        var baseVar = facetConfig.getRootFacetNode().getVar();
+
+        var relation = new sparql.Relation(concept.getElement(), concept.getVar(), baseVar);
+        var countVar = rdf.NodeFactory.createVar('c');
+        var query = sparql.RelationUtils.createQueryDistinctValueCount(relation, countVar);
+
+        query.getOrderBy().push(new sparql.SortCondition(query.getProject().getExpr(countVar), 'desc'));
+        console.log('Resource concept is: ' + query);
+
+
+
+        // TODO Wrap the query with a table mod
+
+
+/*
 
         var facetSystem = new facete.FacetSystemSparql(sparqlService);
         var constraintManager = new facete.ConstraintManager();
-        var facetService = facetSystem.createFacetService(constraintManager);
+        constraintManager.addConstraint(new facete.ConstraintRegex(facete.Path.parse('http://dcat.cc/ontology/groupId'), 'dbpedia'));
+        constraintManager.addConstraint(new facete.ConstraintRegex(facete.Path.parse('http://dcat.cc/ontology/artifacetId'), 'o'));
+*/
+    });
 
-        /*
-        var pathHead = facete.PathHead.parse('', false);
-        facetService.createListService(pathHead).then(function(ls) {
-            return ls.fetchItems('lon');
-        }).then(function(items) {
-            console.log('FACETE: ' + JSON.stringify(items));
-        });
-        */
+    it('#Datacat test', function() {
 
+        if(false) {
+            var sparqlService = new service.SparqlServiceHttp('http://datacat.aksw.org/sparql', []);
+            sparqlService = new service.SparqlServiceConsoleLog(sparqlService);
+
+            var facetSystem = new facete.FacetSystemSparql(sparqlService);
+            var constraintManager = new facete.ConstraintManager();
+            constraintManager.addConstraint(new facete.ConstraintRegex(facete.Path.parse('http://dcat.cc/ontology/groupId'), 'dbpedia'));
+            constraintManager.addConstraint(new facete.ConstraintRegex(facete.Path.parse('http://dcat.cc/ontology/artifacetId'), 'o'));
+
+
+            var facetService = facetSystem.createFacetService(constraintManager);
+        }
+    });
+/*
         var ftc = new facete.FacetTreeConfig();
         ftc.setState(new facete.Path(), new facete.FacetNodeState(1, 10, null, 'funding'));
 
-        ftc.setState(facete.Path.parse('http://fp7-pp.publicdata.eu/ontology/funding'), new facete.FacetNodeState(1, 10, null, null));
+        ftc.setState(facete.Path.parse('http://dcat.cc/ontology/groupId'), new facete.FacetNodeState(1, 10, null, null));
+
 
         facete.FacetTreeService.fetchFacetTree(facetService, ftc).then(function(items) {
-            console.log('PRINTING TREE [START]');
-            items.forEach(function(item) {
-                console.log('TREE: ' + JSON.stringify(item));
-            });
-            console.log('PRINTING TREE [DONE]');
+
+            var json = prettifyFacetTreeChildren(items);
+            console.log('TREE: ' + JSON.stringify(json, null, 4));
         });
-
-/*
-        if(false) {
-            var p = facete.Path.parse('http://fp7-pp.publicdata.eu/ontology/address http://fp7-pp.publicdata.eu/ontology/city');
-            var nv = rdf.NodeFactory.createUri('http://fp7-pp.publicdata.eu/resource/city/Austria-VIENNA');
-            facetConfig.getConstraintManager().addConstraint(new facete.ConstraintEquals(p, nv));
-        }
-
-
-        var sparqlService = new service.SparqlServiceHttp('http://fp7-pp.publicdata.eu/sparql', ['http://fp7-pp.publicdata.eu/']);
-        sparqlService = new service.SparqlServiceConsoleLog(sparqlService);
-
-
-        var bestLabelConfig = new sparql.BestLabelConfig();
-
-        // Transform functions from searchStrings into sparql concepts
-        var fnTransformSearch = function(searchString) {
-
-            var result;
-            if(searchString) {
-
-                var relation = sparql.LabelUtils.createRelationPrefLabels(bestLabelConfig);
-                var result = sparql.KeywordSearchUtils.createConceptRegex(relation, searchString);
-                //var result = sparql.KeywordSearchUtils.createConceptBifContains(relation, searchString);
-            }
-            else {
-                result = null;
-            }
-            return result;
-        };
-
-        //ListServicecreateListServiceMappedConcept
-
-
-        var mappedConcept = sponate.MappedConceptUtils.createMappedConceptBestLabel(bestLabelConfig);
-        var lsBestLabel = sponate.LookupServiceUtils.createLookupServiceMappedConcept(sparqlService, mappedConcept);
-
-        // TODO This is ugly: If a key is not found, a label is derived from the key, but we should add a nicer way for doing this
-        lsBestLabel = new service.LookupServiceTransform(lsBestLabel, function(x) { return x;}, function(node) { return util.UriUtils.extractLabel(node.getUri()) });
-
-        var facetConceptSupplierExact = new facete.FacetConceptSupplierExact(facetConfig);
-        var facetConceptSupplierMeta = new facete.FacetConceptSupplierMeta(facetConceptSupplierExact);
-
-        // TODO: Use the set of declared properties for the root
-        // IF there are no constraints yet
-        //facetConceptSupplierMeta.getPathHeadToConcept().put(new facete.Path(), );
-
-        var facetService = new facete.FacetServiceSparql(sparqlService, facetConceptSupplierMeta);
-        facetService = new facete.FacetServiceTransformConcept(facetService, fnTransformSearch);
-
-        //var path = facete.Path.parse('http://fp7-pp.publicdata.eu/ontology/funding');
-        var pathHead = new facete.PathHead(facete.Path.parse(''), false);
-        var listService = facetService.createListService(pathHead);
-
-
-        listService.fetchItems(null, 10).then(function(properties) {
-            console.log('Got facets', properties);
-
-            var facetRelationIndex = facete.FacetUtils.createFacetRelationIndex(facetConfig, pathHead);
-
-            var lsPreCount = new facete.LookupServiceFacetPreCount(sparqlService, facetRelationIndex);
-            var lsExactCount = new facete.LookupServiceFacetExactCount(sparqlService, facetRelationIndex);
-            var ls = new facete.LookupServiceFacetCount(lsPreCount, lsExactCount);
-
-            ls = lsBestLabel;
-            var promise = ls.lookup(properties);
-            return promise;
-
-
-            /*
-            var properties = [];
-            entries.forEach(function(entry) {
-                properties.push(entry.key);
-            });
-
-            console.log('here');
-            var stepRelations = facete.FacetUtils.createStepRelationsProperties(facetConfig, path, false, [], true);
-            stepRelations.forEach(function(sr) {
-                console.log('StepRelation: ' + sr);
-                console.log('  -- Relation: ' + sparql.RelationUtils.createQueryDistinctValueCount(sr.getRelation(), sparql.VarUtils.c));
-            });
-            * /
-        }).then(function(map) {
-            console.log('FINAL' + JSON.stringify(map.entries()));
-        });
-
-        listService.fetchItems(null, null).then(function(items) {
-            console.log('got everything', items.length);
-        });
-        */
     });
+*/
+
 });
