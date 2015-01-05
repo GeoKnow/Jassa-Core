bcdd42b9f7729a78bcaeec47feb912fcccbb3a13
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-01-05 16:56:53 +0100

    added tests for util.PrefixUtils

--- test/util/PrefixUtils.js
+++ test/util/PrefixUtils.js
@@ -20,6 +20,7 @@ var jassa = require('../../lib')(Promise, ajax);
 var PrefixUtils = require('../../lib/util/PrefixUtils');
 
 describe('PrefixUtils', function() {
+  // extractPrefixes
   it('should extract prefixes correctly', function() {
     var qualifiedName1 = 'ex:sth123';
     var prefix = 'ex';
@@ -38,6 +39,7 @@ describe('PrefixUtils', function() {
     PrefixUtils.extractPrefixes(qualifiedName5).should.eql([prefix]);
   });
 
+  // expandPrefixes
   it('should expand prefixes correctly', function() {
     var prefixMappings = {
       'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
@@ -58,4 +60,36 @@ describe('PrefixUtils', function() {
     var qualifiedName3 = 'unknwn:sth';
     PrefixUtils.expandPrefixes(prefixMappings, qualifiedName3).should.equal(qualifiedName3);
   });
+
+  // parseCurie
+  it('should parse CURIEs correctly', function() {
+    // CURIE examples taken from http://www.w3.org/TR/2010/NOTE-curie-20101216
+    var curie1 = 'home:#start';
+    var expctdRes1 = {key: 'home', 'val': '#start'};
+    var curie2 = 'joseki:';
+    var expctdRes2 = {key: 'joseki', val: ''};
+    var curie3 = "google:xforms+or+'xml+forms'";
+    var expctdRes3 = {key: 'google', val: "xforms+or+'xml+forms'"};
+
+    PrefixUtils.parseCurie(curie1).should.eql(expctdRes1);
+    PrefixUtils.parseCurie(curie2).should.eql(expctdRes2);
+    PrefixUtils.parseCurie(curie3).should.eql(expctdRes3);
+  });
+
+  // parsePrefixDecls
+  it('should parse prefix declaration strings correctly', function() {
+    var prfxDeclStr1 = 'ns: namespace';
+    var expctdPrfxDecl1 = {ns: 'namespace'};
+    var prfxDeclStr2 = 'f: foo b:bar bz: baz';
+    var expctdPrfxDecl2 = {f: 'foo', b: 'bar', bz: 'baz'};
+    var prfxDeclStr3 = '';
+    var expctdPrfxDecl3 = {};
+    var prfxDeclStr4 = 'this is not a prefix declation';
+    var expctdPrfxDecl4 = {};
+
+    PrefixUtils.parsePrefixDecls(prfxDeclStr1).should.eql(expctdPrfxDecl1);
+    PrefixUtils.parsePrefixDecls(prfxDeclStr2).should.eql(expctdPrfxDecl2);
+    PrefixUtils.parsePrefixDecls(prfxDeclStr3).should.eql(expctdPrfxDecl3);
+    PrefixUtils.parsePrefixDecls(prfxDeclStr4).should.eql(expctdPrfxDecl4);
+  })
 });
