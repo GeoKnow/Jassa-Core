a06213aedbc9c89802f7e7f8be20d490fcff86d7
cstadler <cstadler@informatik.uni-leipzig.de>
2015-01-12 12:49:17 +0100

    Added initial benchmark file

---
+++ benchmark/benchmark.js
@@ -0,0 +1,23 @@
+
+// lib includes
+var Promise = require('bluebird');
+var request = Promise.promisifyAll(require('request'));
+var ajax = function(param) {
+  return request.postAsync(param.url, {
+    json: true,
+    form: param.data
+  }).then(function(res) {
+    return new Promise(function(resolve) {
+      resolve(res[0].body);
+    });
+  });
+};
+
+// lib
+var jassa = require('../lib')(Promise, ajax);
+
+
+
+
+console.log(jassa.rdf.NodeFactory.createUri('foo'));
+
