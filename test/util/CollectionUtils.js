6e7e8d3a2fc5c591da59a263ff981b7315ac3549
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2016-01-16 05:49:33 +0100

    Added set union/intersection/difference utils

--- test/util/CollectionUtils.js
+++ test/util/CollectionUtils.js
@@ -21,6 +21,10 @@ var CollectionUtils = require('../../lib/util/CollectionUtils');
 
 var ArrayList = require('../../lib/util/collection/ArrayList');
 
+var HashSet = require('../../lib/util/collection/HashSet');
+var SetUnion = require('../../lib/util/collection/SetUnion');
+var SetIntersection = require('../../lib/util/collection/SetIntersection');
+
 describe('CollectionUtils', function() {
 
   it('should toggle an item\'s membership of a collection correctly', function() {
@@ -39,5 +43,35 @@ describe('CollectionUtils', function() {
     var arrWAppendedItem = [1, 2, 3, 4, 6, 7, 5];
     CollectionUtils.toggleItem(coll, item).should.be.true;
     coll.getArray().should.eql(arrWAppendedItem);
-  })
+  });
+
+  it('should give a proper union view of sets', function() {
+      var a = new HashSet();
+      a.addAll([1, 2, 3]);
+
+      var b = new HashSet();
+      b.addAll([3, 4, 5]);
+
+      var x = new HashSet();
+      x.addAll([1, 2, 3, 4, 5]);
+
+      var c = new SetUnion([a, b]);
+      c.hashCode().should.equal(x.hashCode());
+
+      var y = new HashSet();
+      y.addAll([2, 3, 4, 5]);
+
+      a.remove(1);
+      c.hashCode().should.equal(y.hashCode());
+
+      var z = new HashSet();
+      z.addAll([3]);
+
+      var d = new SetIntersection([a, b]);
+//      console.log('a: ' + a.entries());
+//      console.log('b: ' + b.entries());
+//      console.log('d: ' + d.entries());
+//      console.log('z: ' + z.entries());
+      d.hashCode().should.equal(z.hashCode());
+  });
 });
